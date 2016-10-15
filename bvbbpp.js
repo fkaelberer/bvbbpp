176849245abffa41816ebea8dc07eee8b2db9735
fkaelberer <o_0_o@gmx.de>
2016-10-15 22:02:28 +0200

    Refactor: Extract BvbbLeage to own file

--- bvbbpp.js
+++ bvbbpp.js
@@ -33,58 +33,6 @@ var PREFS = [{
   def: MOBILE
 }];
 
-var BvbbLeague = (function() {
-  var my = {};
-
-  var BB = "BB";
-  var L1 = "LL-1", L2 = "LL-2", L3 = "LL-3", L4 = "LL-4";
-  var Z1 = "BZ-1", Z2 = "BZ-2", Z3 = "BZ-3", Z4 = "BZ-4";
-  var A1 = "AK-1", A2 = "AK-2", A3 = "AK-3", A4 = "AK-4";
-  var B1 = "BK-1", B2 = "BK-2", B3 = "BK-3", B4 = "BK-4";
-  var C1 = "CK-1", C2 = "CK-2", C3 = "CK-3", C4 = "CK-4";
-  var D1 = "DK-1", D2 = "DK-2", D3 = "DK-3", D4 = "DK-4";
-  var E1 = "EK-1", E2 = "EK-2", E3 = "EK-3", E4 = "EK-4";
-  var F1 = "FK-1", F2 = "FK-2", F3 = "FK-3", F4 = "FK-4";
-  var G1 = "GK-1", G2 = "GK-2", G3 = "GK-3", G4 = "GK-4";
-
-  var DIVISIONS = [];
-  my.DIVISIONS = DIVISIONS;
-  // keine Gegenueberstellung 2006/07
-  // DIVISIONS[ 6] = [BB, L1, L2, Z1, Z2, Z3, Z4, A1, A2, A3, A4, B1, B2, B3, C1, C2, C3];
-  // keine Spieltermine 2007/08, 2008/09
-  // DIVISIONS[ 7] = [BB, L1, L2, Z1, Z2, Z3, Z4, A1, A2, A3, A4, B1, B2, B3, B4, C1, C2, C3];
-  // DIVISIONS[ 8] = [BB, L1, L2, Z1, Z2, Z3, Z4, A1, A2, A3, A4, B1, B2, B3, B4, C1, C2, C3];
-
-  // Nur diejenigen Staffeln muessen angegeben werden, die sich von der Vorsaison unterscheiden.
-  DIVISIONS[ 9] = [BB, L1, L2, Z1, Z2, A1, A2, B1, B2, C1, C2, D1, D2, E1, E2, F1, F2, F3];
-  DIVISIONS[11] = [BB, L1, L2, Z1, Z2, A1, A2, B1, B2, C1, C2, D1, D2, E1, E2, F1, F2, G1, G2];
-  DIVISIONS[13] = [BB, L1, L2, Z1, Z2, A1, A2, B1, B2, C1, C2, D1, D2, E1, E2, F1, F2, G1, G2, G3];
-  DIVISIONS[14] = [BB, L1, L2, Z1, Z2, A1, A2, B1, B2, C1, C2, D1, D2, E1, E2, F1, F2, G1, G2];
-
-  var FIRST_SEASON = 9;
-  my.CURRENT_SEASON = getCurrentSeasonYear();
-  my.SEASONS = [];
-  for (var i = FIRST_SEASON; i <= my.CURRENT_SEASON; i++ ) {
-    my.SEASONS.push(i);
-    if (!DIVISIONS[i]) {
-      DIVISIONS[i] = DIVISIONS[i - 1];
-    }
-  }
-
-  function getCurrentSeasonYear() {
-    // lässt die neue Saison ab August starten
-    var d = new Date();
-    var year = d.getFullYear() - 2000;
-    var august = 7;
-    if (d.getMonth() < august) {
-      year -= 1;
-    }
-    return year;
-  }
-
-  return my;
-
-}());
 
 var Styles = (function(){
   function toColorObject(color) {
@@ -115,6 +63,18 @@ var BVBBPP;
 
 
 function Bvbbpp(document) {
+    function getYear(url) {
+        var seasonString = /user_upload\/(\w*)\//.exec(url)[1];
+        if (seasonString === "schuch") {
+            return BvbbLeague.CURRENT_SEASON;
+        }
+        if (seasonString.indexOf("saison") >= 0) {
+            return parseInt(seasonString.substr("saison".length, 2), 10);
+        }
+        return BvbbLeague.CURRENT_SEASON;
+    }
+
+    
   this.URL = document.URL;
   this.valid = true;
   if (!this.valid) {
@@ -199,17 +159,6 @@ Bvbbpp.prototype = {
     }
   }
 };
-
-function getYear(url) {
-  var seasonString = /user_upload\/(\w*)\//.exec(url)[1];
-  if (seasonString === "schuch") {
-    return BvbbLeague.CURRENT_SEASON;
-  }
-  if (seasonString.indexOf("saison") >= 0) {
-    return parseInt(seasonString.substr("saison".length, 2), 10);
-  }
-  return BvbbLeague.CURRENT_SEASON;
-}
 
 function toSeasonName(year) {
   return "20" + twoDigits(year) + "/" + twoDigits(year + 1);
