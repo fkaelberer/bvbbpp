ebaf53af88452bbd4bd012247fdb9d82d1c2fc12
fkaelberer <o_0_o@gmx.de>
2017-07-02 17:25:10 +0200

    extract preferences to separate file

--- bvbbpp.js
+++ bvbbpp.js
@@ -17,23 +17,6 @@ var CALENDAR_ICON = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAA
     "GthSPxz8QqPV6/QGzrR1miJhGE+2to4dFgmU2FAq9IFhgz3DIQRxy6S6tX/kDUgliRvSSn2IAAAAASUVORK5CYII=";
 
 
-
-var MOBILE = false;
-
-// TODO store defaults (relict from old Firefox extension)
-// preferences and defaults
-var PREFS = [{
-  name: "useIframe",
-  def: true
-}, {
-  name: "hideDoodle",
-  def: MOBILE
-}, {
-  name: "hideICS",
-  def: MOBILE
-}];
-
-
 var Styles = (function(){
   function toColorObject(color) {
     return { css: "#" + color, bg: "bg" + color, fg: "col" + color };
@@ -200,40 +183,6 @@ function create() {
   return newElement.apply(this, [document].concat([].slice.call(arguments)));
 }
 
-/**
- * @param name
- *            name of the preference
- * @returns the default value of the preference if it exists, 'undefined' otherwise.
- */
-function getDefaultPrefValue(name) {
-  var defaultPreference = PREFS.find(element => (element.name === name));
-  return defaultPreference ? defaultPreference.def : undefined;
-}
-
-/**
- * Get a preference from the branch "extensions.bvbbpp.". If it doesn't exist, return the preference's
- * default setting. If the default setting doesn't exist either, return undefined.
- *
- * TODO https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Implement_a_settings_page
- *
- * @param name
- *            pref name
- * @param callback
- *            callback function which is called with pref value as argument
- */
-function getPref(name, callback) {
-  if (!callback) return false;
-  return chrome.storage.local.get(name, value => {
-    var prefValue = (value[name] !== undefined) ? value[name] : getDefaultPrefValue(name);
-    callback(prefValue);
-  });
-}
-
-function setPref(name, value) {
-    var pref = {};
-    pref[name] = value;
-    chrome.storage.local.set(pref);
-}
 
 function makeAufstellung() {
   var teamNum = parseInt(BVBBPP.URL.substr(-7, 2), 10);
---
+++ preferences.js
@@ -0,0 +1,50 @@
+var MOBILE = false;
+
+// TODO store defaults (relict from old Firefox extension)
+// preferences and defaults
+var PREFS = [{
+  name: "useIframe",
+  def: true
+}, {
+  name: "hideDoodle",
+  def: MOBILE
+}, {
+  name: "hideICS",
+  def: MOBILE
+}];
+
+
+/**
+ * @param name
+ *            name of the preference
+ * @returns the default value of the preference if it exists, 'undefined' otherwise.
+ */
+function getDefaultPrefValue(name) {
+  var defaultPreference = PREFS.find(element => (element.name === name));
+  return defaultPreference ? defaultPreference.def : undefined;
+}
+
+/**
+ * Get a preference from the branch "extensions.bvbbpp.". If it doesn't exist, return the preference's
+ * default setting. If the default setting doesn't exist either, return undefined.
+ *
+ * TODO https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Implement_a_settings_page
+ *
+ * @param name
+ *            pref name
+ * @param callback
+ *            callback function which is called with pref value as argument
+ */
+function getPref(name, callback) {
+  if (!callback) return false;
+  return chrome.storage.local.get(name, value => {
+    var prefValue = (value[name] !== undefined) ? value[name] : getDefaultPrefValue(name);
+    callback(prefValue);
+  });
+}
+
+function setPref(name, value) {
+    var pref = {};
+    pref[name] = value;
+    chrome.storage.local.set(pref);
+}
