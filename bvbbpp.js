18e3c8860e025bde6262a61ee668f909917cc94d
fkaelberer <o_0_o@gmx.de>
2017-07-02 17:04:49 +0200

    move iCal to separate file and change to ES6 class

--- bvbbpp.js
+++ bvbbpp.js
@@ -686,57 +686,6 @@ function makeDoodleLinks(doc, spiele) {
 }
 
 
-function ICal() {
-  //BEGIN:VCALENDAR
-  //VERSION:2.0
-  //PRODID:http://www.example.com/calendarapplication/
-  //METHOD:PUBLISH
-
-  //BEGIN:VEVENT
-  //UID:461092315540@example.com
-  //ORGANIZER;CN="Alice Balder, Example Inc.":MAILTO:alice@example.com
-  //LOCATION:Somewhere
-  //SUMMARY:Eine Kurzinfo
-  //DESCRIPTION:Beschreibung des Termines
-  //CLASS:PUBLIC
-  //DTSTART:20060910T220000Z
-  //DTEND:20060919T215900Z
-  //DTSTAMP:20060812T125900Z
-  //END:VEVENT
-
-  //END:VCALENDAR
-
-  this.events = [];
-}
-
-ICal.prototype = {
-  toString: function ICal_toString() {
-    var str = [];
-    str.push("BEGIN:VCALENDAR");
-    str.push("VERSION:2.0");
-    str.push("PRODID:https://addons.mozilla.org/de/firefox/addon/bvbbpp/");
-    str.push("METHOD:PUBLISH");
-
-    for (var i = 0; i < this.events.length; i++) {
-      var event = this.events[i];
-
-      str.push("BEGIN:VEVENT");
-      for (var prop in event) {
-        str.push(prop.toUpperCase() + ":" + event[prop]);
-      }
-      str.push("END:VEVENT");
-    }
-
-    str.push("END:VCALENDAR");
-
-    return str.join("\r\n");
-  },
-
-  addEvent: function ICal_addEvent(e) {
-    this.events.push(e);
-  }
-};
-
 function getIconBar(id, table, doc) {
   var iconBar = doc.getElementById("iconBar" + id);
   if (!iconBar) {
