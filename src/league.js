b46996e8e91040b2ba3a4d68ec9078dc8cf85a76
fkaelberer <o_0_o@gmx.de>
2018-01-14 01:10:29 +0100

    Bump version to 1.4.2 and update license date

--- src/league.js
+++ src/league.js
@@ -1,4 +1,4 @@
-// Copyright 2012-2016 Felix Kaelberer
+// Copyright 2012-2018 Felix Kaelberer
 //
 // This work is licensed for reuse under an MIT license. Details are
 // given in the LICENSE file included with this file.
